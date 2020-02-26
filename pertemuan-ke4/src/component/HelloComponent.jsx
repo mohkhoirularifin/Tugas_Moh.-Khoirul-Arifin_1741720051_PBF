7e35ae9305b42312e5dda7c30ba729c395a7b8c7
Moh. Khoirul Arifin <karifin998@gmail.com>
2020-02-26 09:01:01 +0700

    statefullcomponent pada container

---
+++ pertemuan-ke4/src/component/HelloComponent.jsx
@@ -0,0 +1,9 @@
+// agar function arrow HelloComponent bisa berjalan di react maka tambahkan react
+import React from 'react';
+
+const HelloComponent = () => {
+    return <p> Ini Adalah Arrow Function yang Ada di Folder </p>
+}
+
+// agar component ini dapat dipakai dimana saja
+export default HelloComponent;
