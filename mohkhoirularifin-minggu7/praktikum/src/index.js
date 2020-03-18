448a0270b5d80d0c34d454acf8665d73cb247020
Moh. Khoirul Arifin <karifin998@gmail.com>
2020-03-18 10:02:39 +0700

    Installasi react-redux

--- mohkhoirularifin-minggu7/praktikum/src/index.js
+++ mohkhoirularifin-minggu7/praktikum/src/index.js
@@ -4,14 +4,15 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import MainReducer from './reducers/MainReducer'
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
+// import registerServiceWorker from './registerServiceWorker';
+
 
 const store = compose((window.devToolsExtension ? window.devToolsExtension() : f =>
 f))(createStore)(MainReducer)
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));registerServiceWorker();
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));serviceWorker.unregister();;
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
