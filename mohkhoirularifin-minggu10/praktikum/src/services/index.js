5b558f7ef0a9c241c8d1c37c511c61ddb2317414
Moh. Khoirul Arifin <karifin998@gmail.com>
2020-04-08 11:23:54 +0700

    Praktikum 2 - Global API service POST

--- mohkhoirularifin-minggu10/praktikum/src/services/index.js
+++ mohkhoirularifin-minggu10/praktikum/src/services/index.js
@@ -1,3 +1,5 @@
+import Post from "../component/BlogPost/Post";
+
 const domainPath = 'http://localhost:3001';
 const GetAPI = (path) => {
     const promise = new Promise((resolve, reject) => {
@@ -12,10 +14,31 @@ const GetAPI = (path) => {
     return promise;
 }
 
+const PostAPI = (path, data) => {
+    const promise = new Promise((resolve, reject) => {
+        fetch(`${domainPath}/${path}`, {
+            method: 'post',
+            headers: {
+                'Accept' : 'application/json',
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then((result) => {
+            resolve(result);
+        },(err) => {
+            reject(err);
+        })
+    })
+    return promise
+}
+
 const getNewsBlog = () => GetAPI('posts?_sort=id&_order=desc');
+const postNewsBlog = (dataYangDikirim) => PostAPI('posts', dataYangDikirim);
 
 const API = {
-    getNewsBlog
+    getNewsBlog,
+    postNewsBlog
 }
 
 export default API;
