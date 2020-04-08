4b3a331917017e69f336c6f34d71030b37c662ea
Moh. Khoirul Arifin <karifin998@gmail.com>
2020-04-08 11:12:25 +0700

    praktikum 1 - Global API service GET

---
+++ mohkhoirularifin-minggu10/praktikum/src/component/HelloComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './HelloComponent.css'
+
+const HelloComponent = () => {
+    return <p className='text-p'> Ini Adalah Arrow Function yang Ada di Folder Component </p>
+}
+
+export default HelloComponent;
--- mohkhoirularifin-minggu10/praktikum/src/index.js
+++ mohkhoirularifin-minggu10/praktikum/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
+// import App from './App';
+import BlogPost from "./container/BlogPost/BlogPost";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BlogPost />,
   document.getElementById('root')
 );
 
