a655439330cb85ad824c0d718a90375586dae073
Moh. Khoirul Arifin <karifin998@gmail.com>
2020-04-08 14:02:14 +0700

    Praktikum 4 - Manage Global API service

---
+++ mohkhoirularifin-minggu10/praktikum/src/services/get.js
@@ -0,0 +1,16 @@
+import {domainHost, domainPath} from './config'
+
+const GetAPI = (path) => {
+    const promise = new Promise((resolve, reject) => {
+        fetch(`${domainPath}/${path}`)
+            .then(response => response.json())
+            .then((result) => {
+                resolve(result);
+            }, (err) => {
+                reject(err);
+            })
+    })
+    return promise;
+}
+
+export default GetAPI;
