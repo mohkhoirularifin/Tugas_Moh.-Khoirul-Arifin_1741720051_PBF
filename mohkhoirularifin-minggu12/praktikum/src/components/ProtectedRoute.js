b2fb94e6bace314f4c41cbd6b11a66e8007615e2
Moh. Khoirul Arifin <karifin998@gmail.com>
2020-04-23 12:35:13 +0700

    Final

--- mohkhoirularifin-minggu12/praktikum/src/components/ProtectedRoute.js
+++ mohkhoirularifin-minggu12/praktikum/src/components/ProtectedRoute.js
@@ -1,10 +1,27 @@
 import React from "react";
-import { Route } from "react-router-dom";
-
-const ProtectedRoute = ({ component: Component, ...rest }) => {
-    return (
-        <Route {...rest} render = {(props) => <Component {...rest} {...props} />} />
-    );
-};
-
+import { Route, Redirect } from "react-router-dom";
+const ProtectedRoute = ({
+  component: Component,
+  isAuthenticated,
+  isVerifying,
+  ...rest
+}) => (
+  <Route
+    {...rest}
+    render={props =>
+      isVerifying ? (
+        <div />
+      ) : isAuthenticated ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/login",
+            state: { from: props.location }
+          }}
+        />
+      )
+    }
+  />
+);
 export default ProtectedRoute;
