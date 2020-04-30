dc99290c9426f86db489655af4a46f58e0918f94
Moh. Khoirul Arifin <karifin998@gmail.com>
2020-04-30 07:22:29 +0700

    install firebase

--- mohkhoirularifin-minggu13/praktikum/src/index.js
+++ mohkhoirularifin-minggu13/praktikum/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import BlogPost from "./container/BlogPost";
+import BlogPost from "./container/BlogPost/BlogPost";
 
 ReactDOM.render(<BlogPost />, document.getElementById('content'));
 
