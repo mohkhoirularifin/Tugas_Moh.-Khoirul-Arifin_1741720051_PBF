ba127cffe4b4921b18875ba38937227f0958f9bb
Moh. Khoirul Arifin <karifin998@gmail.com>
2020-04-30 08:19:29 +0700

    Edit BlogPost.jsx dan Post.jsx dan membuat file config.js

---
+++ mohkhoirularifin-minggu13/praktikum/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBlfP_CdDgjDwnjfPE34Z6NSGwSRSytq7I",
+    authDomain: "minggu12w.firebaseapp.com",
+    databaseURL: "https://minggu12w.firebaseio.com",
+    projectId: "minggu12w",
+    storageBucket: "minggu12w.appspot.com",
+    messagingSenderId: "715052348730",
+    appId: "1:715052348730:web:4a79ffe8a2cfd6b15e0c03",
+    measurementId: "G-Y0VXS0CRZF"
+};
+
+export default firebaseConfig;
