a7f082dba51663bac9aeeff9f81e94adcb9f7364
Moh. Khoirul Arifin <karifin998@gmail.com>
2020-05-10 14:12:00 +0700

    Create DetailProduk.js

---
+++ mohkhoirularifin-minggu14-uas/src/component/DetailProduk.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DetailProduk extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Detail Produk</h1>
+            </div>
+        )
+    }
+}
