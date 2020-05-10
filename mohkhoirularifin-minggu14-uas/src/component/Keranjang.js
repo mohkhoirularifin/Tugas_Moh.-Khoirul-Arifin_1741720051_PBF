f5c30bcb4dd6244c3d1dac4d9f01187d9e0ed00a
Moh. Khoirul Arifin <karifin998@gmail.com>
2020-05-10 14:11:48 +0700

    Create Keranjang.js

---
+++ mohkhoirularifin-minggu14-uas/src/component/Keranjang.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Keranjang extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Keranjang</h1>
+            </div>
+        )
+    }
+}
