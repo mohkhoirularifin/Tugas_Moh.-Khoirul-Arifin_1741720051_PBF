34eece2e54de277754fd13e9119ab3632f3a92c7
Moh. Khoirul Arifin <karifin998@gmail.com>
2020-05-10 14:11:40 +0700

    Create ListProduk.js

---
+++ mohkhoirularifin-minggu14-uas/src/component/ListProduk.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ListProduk extends Component {
+    render() {
+        return (
+            <div>
+                <h1>List Produk</h1>
+            </div>
+        )
+    }
+}
